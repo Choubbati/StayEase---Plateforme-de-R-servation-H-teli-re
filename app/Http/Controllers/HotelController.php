f390e5f9606b9a984835c487aefa1ba92f4d7c66
n1o2h <aythmadnhylt@gmail.com>
2026-02-03 23:35:46 +0100

    ajout du crud pour hotel

--- app/Http/Controllers/HotelController.php
+++ app/Http/Controllers/HotelController.php
@@ -1,10 +1,67 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use App\Models\Hotel;
 use Illuminate\Http\Request;
 
 class HotelController extends Controller
 {
-    //
+
+    public function index(){
+        $hotels = Hotel::all();
+        return view('hotels.Hotels', compact('hotels'));
+    }
+
+    public function create(){
+        return view('hotels.create');
+    }
+
+    public function store(Request $request){
+        $validatedHotel = $request->validate([
+            'nom' => 'required',
+            'description' => 'required',
+            'ville' => 'required',
+            'image' => 'required',
+        ]);
+        Hotel::create($validatedHotel);
+        return redirect()->route('hotels.index');
+    }
+
+    public function edit($hotel){
+
+        if($hotel){
+            $hotel = Hotel::find($hotel);
+        }
+
+        return view('hotels.edit', compact('hotel'));
+    }
+
+    public function update(Request $request, $hotel){
+
+        if($hotel){
+            $hotel = Hotel::find($hotel);
+        }
+
+        $validatedHotel = $request->validate([
+            'nom' => 'required',
+            'description' => 'required',
+            'ville' => 'required',
+            'image' => 'required'
+        ]);
+
+        $hotel->update($validatedHotel);
+        return redirect()->route('hotels.index');
+    }
+
+    public function destroy($hotel){
+
+        if($hotel){
+            $hotel = Hotel::find($hotel);
+        }
+
+        $hotel->delete();
+        return redirect()->route('hotels.index');
+
+    }
+
 }
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
-
+use App\Http\Controllers\HotelController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/hotels/hotels', [HotelController::class, 'index']);
+Route::get('/hotels/create', [HotelController::class, 'create']);
+
+
