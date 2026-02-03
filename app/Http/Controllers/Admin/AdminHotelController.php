043d5e6999a6bcab31cfbcd187718fdf5d8da4a6
chouaib <choubbati@gmail.com>
2026-02-03 17:25:22 +0100

    migrate table hotel et ajouter model hotel

---
+++ app/Http/Controllers/Admin/AdminHotelController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class AdminHotelController extends Controller
+{
+    // public function pending(){
+    //     $hotels = Hotel::where
+    // }
+}
--- routes/web.php
+++ routes/web.php
@@ -6,8 +6,8 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::middleware(['auth', 'role:admin'])->prefix('admin')->group(function() {
-    Route::get("/hotels/pending", [AdminHotelController::class,'pending']->name('admin.hotels.pending'));
-    Route::put("/hotels/{hotel}/aprove",[AdminHotelController::class,'aprove']->name('admin.hotls.aprove'));
-    Route::put("/hotels/{hotel}/reject",[AdminHotelController::class,'reject']->name('admin.hotels.reject'));
-});
+// Route::middleware(['auth', 'role:admin'])->prefix('admin')->group(function() {
+//     Route::get("/hotels/pending", [AdminHotelController::class,'pending']->name('admin.hotels.pending'));
+//     Route::put("/hotels/{hotel}/aprove",[AdminHotelController::class,'aprove']->name('admin.hotls.aprove'));
+//     Route::put("/hotels/{hotel}/reject",[AdminHotelController::class,'reject']->name('admin.hotels.reject'));
+// });
