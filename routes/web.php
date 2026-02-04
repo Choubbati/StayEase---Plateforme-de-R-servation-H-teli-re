eda57c8a3b48966ee422dfaa024e5f829d2445fd
n1o2h <aythmadnhylt@gmail.com>
2026-02-04 11:58:42 +0100

    ajout des routes pour hotel

--- routes/web.php
+++ routes/web.php
@@ -4,5 +4,11 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/hotels/hotels', [HotelController::class, 'index']);
 Route::get('/hotels/create', [HotelController::class, 'create']);
+Route::post('/hotels/hotels', [HotelController::class, 'store']);
+Route::get('/hotels/{hotel}', [HotelController::class, 'edit']);
+Route::get('/hotels/{hotel}', [HotelController::class, 'update']);
+Route::get('/hotels/{hotel}', [HotelController::class, 'destroy']);
+
+
 
 
