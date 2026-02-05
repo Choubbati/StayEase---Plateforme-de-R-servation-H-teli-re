7639beb41e83fbf36969926e9a5e61e5c25bf569
n1o2h <aythmadnhylt@gmail.com>
2026-02-05 07:10:30 +0100

    ajout de middelware pour la gestion des hotels et leur crud et l'ajout des templates pour gestion d'hotel et creation et modification et ajout du fichier api.php vide

--- app/Http/Controllers/GerantHotelController.php
+++ app/Http/Controllers/GerantHotelController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 use App\Models\Hotel;
 use Illuminate\Http\Request;
 
-class HotelController extends Controller
+class GerantHotelController extends Controller
 {
 
     public function index(){
@@ -25,7 +25,7 @@ class HotelController extends Controller
         ]);
         $hotels= Hotel::create($validatedHotel);
 //        dd($hotels);
-        return redirect()->route('hotels.index');
+        return redirect()->route('hotels.hotels');
     }
 
     public function edit($hotel){
@@ -51,7 +51,7 @@ class HotelController extends Controller
         ]);
 
         $hotel->update($validatedHotel);
-        return redirect()->route('hotels.index');
+        return redirect()->route('hotels.hotels');
     }
 
     public function destroy($hotel){
@@ -61,7 +61,7 @@ class HotelController extends Controller
         }
 
         $hotel->delete();
-        return redirect()->route('hotels.index');
+        return redirect()->route('hotels.hotels');
 
     }
 
