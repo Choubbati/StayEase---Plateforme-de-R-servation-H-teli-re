f310ce049dd16994221bde456aff3dae1b6fb749
Moamiine <medamine.teljaoui@gmail.com>
2026-02-09 16:01:15 +0100

    added showUsers method to AdminController for displaying all users

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -3,12 +3,17 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
 
 class AdminController extends Controller
 {
     public function index(){
 
     return view('admin.adminDashboard');
+}   
+    public function showUsers(){
+        $users = User::all();
+        return view('admin.users.index', compact('users'));
+    }
+    
 }
-}
-
