f4668eead5704b09ee5b3b0ec5f5627672ecfca0
Moamiine <medamine.teljaoui@gmail.com>
2026-02-10 17:59:10 +0100

    added isValidated to register and login , so if a user with role_id = 2 , he will be redirected to a page that he musts wait for the admin validation so he can get the role

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -9,52 +9,63 @@ use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
-    public function register(Request $request){
+    public function register(Request $request)
+    {
         // $request->validate([
         //     'name' => 'required|string|max:255',
         //     'email' => 'required|string|email|unique:users',
         //     'password' => 'required|string|min:8',
         //     ]);
-        
+
         $user = User::create([
             'Firstname' => $request->prenom,
             'Lastname' => $request->nom,
-            'email' =>$request->email,
-            'password' =>Hash::make($request->password),
-            'role_id' => 3,
-            ]);
-            
-        Auth::login($user);
-        return redirect()->route('home');
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+            'role_id' => $request->role_id,
+            'isValidated' => ($request->role_id == 2) ? false : true
+        ]);
 
+        if ($user->isValidated == true) {
+            Auth::login($user);
+            return redirect()->route('home');
+        } else {
+            return view('wait');
+        }
     }
 
-    public function login(Request $request){
-        
-    $validated = $request->validate([
-        'email' => 'required|email',
-        'password' => 'required|string',
-    ]);
 
-    if(Auth::attempt($validated)){
-        $request->session()->regenerate();
+    public function login(Request $request)
+    {
 
-        if (Auth::user()->role_id == 1) { 
-        return redirect()->route('admin.dashboard');
+        $validated = $request->validate([
+            'email' => 'required|email',
+            'password' => 'required|string',
+        ]);
+        if (Auth::attempt($validated)) {
+            if (Auth::user()->isValidated) {
+
+                $request->session()->regenerate();
+                  if (Auth::user()->role_id == 1) {
+                        return redirect()->route('admin.dashboard');
+                  }
+                if (Auth::user()->isValidated == true) {
+                    return redirect()->route('home');
+                    } else {
+                        return view('wait');
+                    }
+            }
+                
+
+        }
     }
 
-        return redirect()->intended('/');
-        
-    }
 
-    }
-
-    public function logout(Request $request){
+    public function logout(Request $request)
+    {
         Auth::logout();
-            $request->session()->invalidate();              
-            $request->session()->regenerateToken();
-            return redirect()->intended('/');
+        $request->session()->invalidate();
+        $request->session()->regenerateToken();
+        return redirect()->intended('/');
     }
-
-
 }
